c7d0059007914f593c61d1c6cb15317149ad286a
Brian <bstanton773@gmail.com>
2022-11-08 13:13:14 -0600

    Added handle form submit

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ToDoCard from './ToDoCard';
 import ToDoForm from './ToDoForm';
 
 export default function ToDo(props) {
-    let tasks = ['Create React App', 'Add Components', 'Display Cards', 'Allow Submissions'];
+    const [tasks, setTasks] = useState([]);
+
+    function handleFormSubmit(event){
+        event.preventDefault();
+        let newTask = event.target.task.value;
+        setTasks([...tasks, newTask])
+        event.target.task.value = '';
+    }
+
     return (
         <>
             <h1 className="text-center">Brian's Awesome To-Do List Generator</h1>
-            <ToDoForm />
-            {/* To-Do's */}
+            <ToDoForm handleFormSubmit={handleFormSubmit}/>
             <div className="row">
                 {tasks.map((task, idx) => <ToDoCard task={task} key={idx} />)}
             </div>
--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ToDoForm(props) {
     return (
-        <form className="my-3">
+        <form className="my-3" onSubmit={props.handleFormSubmit}>
             <div className="form-group">
                 <input type="text" name="task" className="form-control" placeholder="Enter New To-Do" />
                 <input type="submit" className="btn btn-danger w-100 mt-3" value="Add To-Do" />
